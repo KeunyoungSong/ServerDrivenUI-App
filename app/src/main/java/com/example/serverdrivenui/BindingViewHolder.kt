bce9e674cc38c8ff54df53a5c148bf4e3d09961e
LB-Brandon <rmsdud623@gmail.com>
2024-05-22 17:39:14 +0900

    RecyclerView의 데이터 바인딩을 위한 BindingViewHolder 클래스 추가
    - ViewDataBinding을 제네릭으로 사용하는 추상 클래스 정의
    - ListItem을 바인딩하고 RecyclerView에 표시하는 기본 기능 구현
    - bind 메서드에서 ListItem을 설정하고 데이터 바인딩 변수 업데이트

---
+++ app/src/main/java/com/example/serverdrivenui/BindingViewHolder.kt
@@ -0,0 +1,16 @@
+package com.example.serverdrivenui
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.serverdrivenui.model.ListItem
+
+abstract class BindingViewHolder<VB : ViewDataBinding>(
+    private val binding: VB
+) : RecyclerView.ViewHolder(binding.root) {
+    protected var item: ListItem? = null
+
+    open fun bind(item: ListItem) {
+        this.item = item
+        binding.setVariable(BR.item, this.item)
+    }
+}
