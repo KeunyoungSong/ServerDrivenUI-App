580990f4cbca30c24ca7fec6ae1da14bec5d585a
LB-Brandon <rmsdud623@gmail.com>
2024-05-22 18:08:40 +0900

    ListAdapter 클래스 추가 및 뷰 홀더 생성 로직 구현
    - 서버 주도 UI 를 위한 ListAdapter 클래스 구현

---
+++ app/src/main/java/com/example/serverdrivenui/ListAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.serverdrivenui
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.serverdrivenui.model.ListItem
+
+class ListAdapter : ListAdapter<ListItem, BindingViewHolder<*>>(DiffCallback()) {
+
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return item?.viewType?.ordinal ?: -1
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<*> {
+        return ViewHolderGenerator.get(parent, viewType)
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder<*>, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item)
+        }
+    }
+}
