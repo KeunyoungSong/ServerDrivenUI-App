93047ac4725db3ddfaf6f3259d7447220d4738c4
LB-Brandon <rmsdud623@gmail.com>
2024-05-22 18:17:34 +0900

    ViewPager 데이터 클래스 생성
    - 서버 주도 UI에서 뷰 페이저 아이템을 표현하기 위한 ViewPager 데이터 클래스를 추가했습니다

---
+++ app/src/main/java/com/example/serverdrivenui/model/ViewPager.kt
@@ -0,0 +1,8 @@
+package com.example.serverdrivenui.model
+
+data class ViewPager(
+    val items: List<ListItem>
+) : ListItem{
+    override val viewType: ViewType
+        get() = ViewType.VIEW_PAGER
+}
