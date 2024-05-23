4b9d1e66dc30ca845776463381415a1bb508a2bb
LB-Brandon <rmsdud623@gmail.com>
2024-05-23 18:10:45 +0900

    Paging3 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("androidx.paging:paging-runtime-ktx:3.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
