6a82b0242933da34e0049612b46decd8eabc6f14
LB-Brandon <rmsdud623@gmail.com>
2024-05-23 13:18:57 +0900

    ViewPagerViewHolder 추가

---
+++ app/src/main/java/com/example/serverdrivenui/viewholder/ViewPagerViewHolder.kt
@@ -0,0 +1,23 @@
+package com.example.serverdrivenui.viewholder
+
+import com.example.serverdrivenui.adapter.ListAdapter
+import com.example.serverdrivenui.databinding.ItemViewpagerBinding
+import com.example.serverdrivenui.model.ListItem
+import com.example.serverdrivenui.model.viewtypeholder.ViewPager
+
+class ViewPagerViewHolder(
+    binding: ItemViewpagerBinding
+) : BindingViewHolder<ItemViewpagerBinding>(binding){
+
+    private val adapter = ListAdapter()
+
+    init {
+        binding.viewpager.adapter = adapter
+    }
+
+    override fun bind(item: ListItem) {
+        super.bind(item)
+        item as ViewPager
+        adapter.submitList(item.items)
+    }
+}
