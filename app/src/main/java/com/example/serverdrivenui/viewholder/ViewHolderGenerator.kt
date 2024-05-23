3c164a4ba6524c81fbb278abee9094610a5b60e8
LB-Brandon <rmsdud623@gmail.com>
2024-05-23 15:38:54 +0900

    ViewHolderGenerator 에 item 관련 ViewHolder 추가(Coupon, Image, SellItem, Sale

--- app/src/main/java/com/example/serverdrivenui/viewholder/ViewHolderGenerator.kt
+++ app/src/main/java/com/example/serverdrivenui/viewholder/ViewHolderGenerator.kt
@@ -8,6 +8,10 @@ import com.example.serverdrivenui.model.ViewType
 import com.example.serverdrivenui.viewholder.category.FullAdViewHolder
 import com.example.serverdrivenui.viewholder.category.HorizontalViewHolder
 import com.example.serverdrivenui.viewholder.category.ViewPagerViewHolder
+import com.example.serverdrivenui.viewholder.item.CouponViewHolder
+import com.example.serverdrivenui.viewholder.item.ImageViewHolder
+import com.example.serverdrivenui.viewholder.item.SaleViewHolder
+import com.example.serverdrivenui.viewholder.item.SellItemViewHolder
 
 object ViewHolderGenerator {
 
@@ -16,30 +20,33 @@ object ViewHolderGenerator {
      * 새로운 뷰타입 사용시 뷰 타입 추가 필요
      */
     fun get(
-        parent: ViewGroup,
-        viewType: Int
+        parent: ViewGroup, viewType: Int
     ): BindingViewHolder<*> {
-        return when(viewType){
+        return when (viewType) {
+            // 카테고리 뷰타입
             ViewType.VIEW_PAGER.ordinal -> ViewPagerViewHolder(parent.toBinding())
             ViewType.HORIZONTAL.ordinal -> HorizontalViewHolder(parent.toBinding())
             ViewType.FULL_AD.ordinal -> FullAdViewHolder(parent.toBinding())
+            // 아이템 뷰타입
+            ViewType.COUPON.ordinal -> CouponViewHolder(parent.toBinding())
+            ViewType.IMAGE.ordinal -> ImageViewHolder(parent.toBinding())
+            ViewType.SELL_ITEM.ordinal -> SellItemViewHolder(parent.toBinding())
+            ViewType.SALE.ordinal -> SaleViewHolder(parent.toBinding())
+            // 미지원 타입
             else -> ItemViewHolder(parent.toBinding())
         }
     }
 
-    class ItemViewHolder(binding:ItemEmptyBinding): BindingViewHolder<ItemEmptyBinding>(binding)
+    class ItemViewHolder(binding: ItemEmptyBinding) : BindingViewHolder<ItemEmptyBinding>(binding)
 
     /**
      * 이 함수는 제네릭 리플렉션을 사용하여 ViewBinding 객체를 생성합니다.
      * ViewGroup 객체에서 호출되며,LayoutInflater와 함께 사용하여 뷰 바인딩 객체를 동적으로 생성합니다.
      * 이는 다양한 뷰 바인딩 클래스를 유연하게 처리할 수 있도록 합니다.
      */
-    private inline fun<reified V:ViewBinding> ViewGroup.toBinding() :V {
+    private inline fun <reified V : ViewBinding> ViewGroup.toBinding(): V {
         return V::class.java.getMethod(
-            "inflate",
-            LayoutInflater::class.java,
-            ViewGroup::class.java,
-            Boolean::class.java
+            "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
         ).invoke(null, LayoutInflater.from(context), this, false) as V
     }
 }
