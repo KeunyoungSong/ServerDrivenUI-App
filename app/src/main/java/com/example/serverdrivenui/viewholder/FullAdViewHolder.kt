2b58f3efb57c7cd4906ff993fdd3c6951d65fd2e
LB-Brandon <rmsdud623@gmail.com>
2024-05-23 13:23:46 +0900

    FullAdViewHolder 추가

---
+++ app/src/main/java/com/example/serverdrivenui/viewholder/FullAdViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.serverdrivenui.viewholder
+
+import com.example.serverdrivenui.databinding.ItemFullAdBinding
+
+class FullAdViewHolder(
+    val binding:ItemFullAdBinding
+) : BindingViewHolder<ItemFullAdBinding>(binding)
+
+// xml 수준에서 UI구현이 처리되기 때문에 코드로 추가 처리하지 않음
