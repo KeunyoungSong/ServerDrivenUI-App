62e5ed1665fd5a97123045de7c2bbfd59323037d
LB-Brandon <rmsdud623@gmail.com>
2024-05-27 15:00:01 +0900

    Deserialize 로직 변경

--- app/src/main/java/com/example/serverdrivenui/remote/ListItemDeserializer.kt
+++ app/src/main/java/com/example/serverdrivenui/remote/ListItemDeserializer.kt
@@ -10,7 +10,6 @@ import com.example.serverdrivenui.model.item.Coupon
 import com.example.serverdrivenui.model.item.Image
 import com.example.serverdrivenui.model.item.Sale
 import com.example.serverdrivenui.model.item.SellItem
-import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
@@ -19,30 +18,24 @@ import java.lang.reflect.Type
 
 class ListItemDeserializer : JsonDeserializer<ListItem> {
     override fun deserialize(
-        json: JsonElement,
-        typeOfT: Type,
-        context: JsonDeserializationContext
+        json: JsonElement, typeOfT: Type, context: JsonDeserializationContext
     ): ListItem {
         val viewTypeName = json.asJsonObject.getAsJsonPrimitive("viewType").asString
-        // 내부 필드 변환을 위한
-        val gson = GsonBuilder()
-            .registerTypeAdapter(ListItem::class.java, ListItemDeserializer()).create()
-
         return try {
             return when (viewTypeName) {
-                ViewType.VIEW_PAGER.name -> gson.fromJson(json, ViewPager::class.java)
-                ViewType.HORIZONTAL.name -> gson.fromJson(json, Horizontal::class.java)
-                ViewType.FULL_AD.name -> gson.fromJson(json, FullAd::class.java)
+                ViewType.VIEW_PAGER.name -> context.deserialize(json, ViewPager::class.java)
+                ViewType.HORIZONTAL.name -> context.deserialize(json, Horizontal::class.java)
+                ViewType.FULL_AD.name -> context.deserialize(json, FullAd::class.java)
 
-                ViewType.SELL_ITEM.name -> gson.fromJson(json, SellItem::class.java)
-                ViewType.IMAGE.name -> gson.fromJson(json, Image::class.java)
-                ViewType.SALE.name -> gson.fromJson(json, Sale::class.java)
-                ViewType.COUPON.name -> gson.fromJson(json, Coupon::class.java)
+                ViewType.SELL_ITEM.name -> context.deserialize(json, SellItem::class.java)
+                ViewType.IMAGE.name -> context.deserialize(json, Image::class.java)
+                ViewType.SALE.name -> context.deserialize(json, Sale::class.java)
+                ViewType.COUPON.name -> context.deserialize(json, Coupon::class.java)
 
-                else -> gson.fromJson(json, Empty::class.java)
+                else -> context.deserialize(json, Empty::class.java)
             }
         } catch (e: Exception) {
-            gson.fromJson(json, Empty::class.java)
+            context.deserialize(json, Empty::class.java)
         }
     }
 }
