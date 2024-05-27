293a98653e737d0c3a34ba4dcef9fd45d281822a
LB-Brandon <rmsdud623@gmail.com>
2024-05-27 10:16:54 +0900

    PagingAdapter 구현

---
+++ app/src/main/java/com/example/serverdrivenui/adapter/PagingListAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.serverdrivenui.adapter
+
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import com.example.serverdrivenui.databinding.model.ListItem
+import com.example.serverdrivenui.databinding.viewholder.BindingViewHolder
+import com.example.serverdrivenui.databinding.viewholder.ViewHolderGenerator
+
+class PagingListAdapter : PagingDataAdapter<ListItem, BindingViewHolder<*>>(DiffCallback()) {
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return item?.viewType?.ordinal ?: -1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<*> {
+        return ViewHolderGenerator.get(parent, viewType)
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder<*>, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item)
+        }
+    }
+
+}
